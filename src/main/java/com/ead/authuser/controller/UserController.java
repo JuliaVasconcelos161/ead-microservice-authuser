d20223220934209a5818c63b222d86b404224c8b
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-04-26 23:14:35 -0300

    Criando os métodos getOne, getAll e delete para User

---
+++ src/main/java/com/ead/authuser/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.ead.authuser.controller;
+
+import com.ead.authuser.model.UserModel;
+import com.ead.authuser.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge=3600)
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers(){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value = "userId") UUID userId) {
+        Optional<UserModel> userModelOptional = userService.findByid(userId);
+        if(userModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable(value = "userId") UUID userId){
+        Optional<UserModel> userModelOptional = userService.findByid(userId);
+        if(userModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        userService.delete(userModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
+    }
+}
--- src/main/java/com/ead/authuser/repository/UserRepository.java
+++ src/main/java/com/ead/authuser/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.repository;
 
 import com.ead.authuser.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
