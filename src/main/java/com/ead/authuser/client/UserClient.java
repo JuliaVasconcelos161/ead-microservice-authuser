64143287a9c88cca7daaaf0d48ba4c76d305a639
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-06-26 18:19:59 -0300

    Adaptando para tornar o código mais limpo

--- src/main/java/com/ead/authuser/client/UserClient.java
+++ src/main/java/com/ead/authuser/client/UserClient.java
@@ -2,20 +2,20 @@ package com.ead.authuser.client;
 
 import com.ead.authuser.model.dto.CourseDto;
 import com.ead.authuser.model.dto.ResponsePageDto;
+import com.ead.authuser.service.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.UUID;
 @Log4j2
@@ -24,25 +24,17 @@ public class UserClient {
 
     private final RestTemplate restTemplate;
 
-    StringBuilder REQUEST_URI = new StringBuilder("http://localhost:8082");
+    private final UtilsService utilsService;
 
-    public UserClient(RestTemplate restTemplate) {
+    public UserClient(RestTemplate restTemplate, UtilsService utilsService) {
         this.restTemplate = restTemplate;
+        this.utilsService = utilsService;
     }
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
-        StringBuilder url = REQUEST_URI;
-        url.append("/courses?userId=");
-        url.append(userId);
-        url.append("&page=");
-        url.append(pageable.getPageNumber());
-        url.append("&size=");
-        url.append(pageable.getPageSize());
-        url.append("&sort=");
-        url.append(pageable.getSort().toString().replaceAll(": ", ","));
-        String urlString = url.toString();
+        String urlString = utilsService.createUrl(userId, pageable);
         log.debug("Request Url: {} ", urlString);
         log.info("Request Url: {} ", urlString);
         try{
