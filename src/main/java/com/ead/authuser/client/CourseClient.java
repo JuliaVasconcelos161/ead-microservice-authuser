7ca94ac919acb3a43ba6a6c590196c583851012e
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-07-09 17:55:04 -0300

    Colocando valor da URL que comunica com o microservice course no application.yaml

--- src/main/java/com/ead/authuser/client/CourseClient.java
+++ src/main/java/com/ead/authuser/client/CourseClient.java
@@ -4,9 +4,9 @@ import com.ead.authuser.model.dto.CourseDto;
 import com.ead.authuser.model.dto.ResponsePageDto;
 import com.ead.authuser.service.UtilsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +20,16 @@ import java.util.List;
 import java.util.UUID;
 @Log4j2
 @Component
-public class UserClient {
+public class CourseClient {
 
     private final RestTemplate restTemplate;
 
     private final UtilsService utilsService;
 
-    public UserClient(RestTemplate restTemplate, UtilsService utilsService) {
+    @Value("${ead.api.url.course}")
+    private String REQUEST_URL_COURSE;
+
+    public CourseClient(RestTemplate restTemplate, UtilsService utilsService) {
         this.restTemplate = restTemplate;
         this.utilsService = utilsService;
     }
@@ -34,7 +37,7 @@ public class UserClient {
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
-        String urlString = utilsService.createUrl(userId, pageable);
+        String urlString = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
         log.debug("Request Url: {} ", urlString);
         log.info("Request Url: {} ", urlString);
         try{
