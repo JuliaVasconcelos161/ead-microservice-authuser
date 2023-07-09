370ceec6481a5140d6b4e59daa4c9e39a150e313
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-07-09 18:28:12 -0300

    Criando UserCourseDto

--- src/main/java/com/ead/authuser/controller/UserCourseController.java
+++ src/main/java/com/ead/authuser/controller/UserCourseController.java
@@ -2,6 +2,8 @@ package com.ead.authuser.controller;
 
 import com.ead.authuser.client.CourseClient;
 import com.ead.authuser.model.dto.CourseDto;
+import com.ead.authuser.model.dto.ResponsePageDto;
+import com.ead.authuser.model.dto.UserCourseDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +11,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Log4j2
@@ -33,4 +33,8 @@ public class UserCourseController {
             @PathVariable(value = "userId") UUID userId) {
         return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
     }
+
+    @PostMapping("/users/{userId}/courses/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "userId") UUID userId,
+                                                               @RequestBody @Valid UserCourseDto userCourseDto)
 }
