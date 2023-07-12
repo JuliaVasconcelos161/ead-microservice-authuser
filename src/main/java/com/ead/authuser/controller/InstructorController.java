3823af605cec4fc9f10476a48b73a34164d263b1
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-07-12 19:23:26 -0300

    Criando um método para salvar a inscrição de um instrutor

---
+++ src/main/java/com/ead/authuser/controller/InstructorController.java
@@ -0,0 +1,41 @@
+package com.ead.authuser.controller;
+
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.model.UserModel;
+import com.ead.authuser.model.dto.InstructorDto;
+import com.ead.authuser.service.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/instructors")
+public class InstructorController {
+
+    private final UserService userService;
+
+    public InstructorController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/subscription")
+    public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
+        Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
+        if(!userModelOptional.isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+
+        var userModel = userModelOptional.get();
+        userModel.setUserType(UserType.INSTRUCTOR);
+        userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.OK).body(userModel);
+    }
+}
+
