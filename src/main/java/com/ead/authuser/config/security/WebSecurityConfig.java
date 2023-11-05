d0b40b89c049cef8708545141ddc10c6b142ec24
JuliaVasconcelos161 <juliavasconceloscrib@gmail.com>
2023-11-05 20:57:09 -0300

    feat: implementando filtro que verifica se token é valido

--- src/main/java/com/ead/authuser/config/security/WebSecurityConfig.java
+++ src/main/java/com/ead/authuser/config/security/WebSecurityConfig.java
@@ -2,16 +2,17 @@ package com.ead.authuser.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,27 +23,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AuthenticationEntryPointImpl authenticationEntryPoint;
 
+    private final AuthenticationJwtFilter authenticationJwtFilter;
+
     private static final String[] AUTH_WHITELIST = {
             "/auth/**"
     };
 
-    public WebSecurityConfig(AuthenticationEntryPointImpl authenticationEntryPoint, UserDetailsService userDetailsService) {
+    public WebSecurityConfig(AuthenticationEntryPointImpl authenticationEntryPoint, UserDetailsService userDetailsService, AuthenticationJwtFilter authenticationJwtFilter) {
         this.authenticationEntryPoint = authenticationEntryPoint;
         this.userDetailsService = userDetailsService;
+        this.authenticationJwtFilter = authenticationJwtFilter;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
-                .authenticationEntryPoint(authenticationEntryPoint)
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers(AUTH_WHITELIST).permitAll()
-                .antMatchers(HttpMethod.GET, "/users/**").hasRole("STUDENT")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+        http.addFilterBefore(authenticationJwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
     @Bean
     @Override
